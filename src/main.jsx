1fc21407fca8454b147c27ca6b6f4c5453c0122c
Daniel-rod99 <danielrodriguez246@gmail.com>
2024-10-08 22:28:00 +0200

    Update Proyect

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import { AnimatePresence } from "framer-motion";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Builder from "./Builder.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
       <AnimatePresence>
