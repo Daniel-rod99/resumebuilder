a0ed8106922a7664c04d0708e86b389364b357e4
Daniel-rod99 <danielrodriguez246@gmail.com>
2024-10-08 21:56:31 +0200

    Update Proyect

--- src/components/FormBuilder.jsx
+++ src/components/FormBuilder.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { Button, Input } from "@nextui-org/react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { FaArrowRight } from "react-icons/fa";
 import { PiUserCirclePlusLight } from "react-icons/pi";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { DateInput } from "@nextui-org/react";
+import { CalendarDate } from "@internationalized/date";
 
 export default function FormBuilder() {
   const navigate = useNavigate();
   const handleNavigateApp = () => {
     navigate(-1);
+  };
+
+  const [showMoreDetails, setShowMoreDetails] = useState(false);
+  const toggleShowMoreDetails = () => {
+    setShowMoreDetails(!showMoreDetails);
   };
 
   return (
@@ -25,32 +33,57 @@ export default function FormBuilder() {
               <PiUserCirclePlusLight className="transition cursor-pointer hover:scale-110" />
             </div>
             <div className="flex flex-col gap-3 lg:flex-row">
-              <Input type="text" label="First Name" isRequired />
-              <Input type="text" label="Last Name" isRequired />
+              <Input type="text" label="First Name" isRequired isClearable />
+              <Input type="text" label="Last Name" isRequired isClearable />
             </div>
             <div className="flex flex-col gap-3 lg:flex-row">
-              <Input type="email" label="Email" isRequired />
-              <Input type="phone" label="Phone" />
+              <Input type="email" label="Email" isRequired isClearable />
+              <Input type="phone" label="Phone" isClearable />
             </div>
-            <Input type="text" label="Address" />
+            <Input type="text" label="Address" isClearable />
           </CardBody>
           <CardFooter>
-            <h1 className="flex items-center gap-1 cursor-pointer hover:text-blue-500">
-              Add More Details <IoIosArrowDropdownCircle />
-            </h1>
+            <motion.h1
+              onClick={toggleShowMoreDetails}
+              className="flex items-center gap-1 cursor-pointer hover:text-blue-500"
+            >
+              Add More Details
+              <motion.span
+                initial={{ rotate: 0 }}
+                animate={{ rotate: showMoreDetails ? 180 : 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <IoIosArrowDropdownCircle />
+              </motion.span>
+            </motion.h1>
           </CardFooter>
-          <CardBody>
-            <motion.div className="gap-3">
-              <div className="flex flex-col gap-3 lg:flex-row">
-                <Input type="text" label="City" />
-                <Input type="text" label="Postal Code" />
-              </div>
-              <div className="flex flex-col gap-3 lg:flex-row">
-                <Input type="text" label="Nationality" />
-                <Input type="text" label="Date Of Birth" />
-              </div>
-            </motion.div>
-          </CardBody>
+
+          <AnimatePresence>
+            {showMoreDetails && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <CardBody className="gap-3">
+                  <div className="flex flex-col gap-3 lg:flex-row">
+                    <Input type="text" label="City" isClearable />
+                    <Input type="number" label="Postal Code" isClearable />
+                  </div>
+                  <div className="flex flex-col gap-3 lg:flex-row">
+                    <Input type="text" label="Nationality" isClearable />
+                    <DateInput
+                      label={"Birth date"}
+                      placeholderValue={new CalendarDate(1995, 11, 6)}
+                      errorMessage="Please enter a valid date."
+                      isClearable
+                    />
+                  </div>
+                </CardBody>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </Card>
       </motion.div>
       <motion.div className="flex mt-6 justify-evenly">
